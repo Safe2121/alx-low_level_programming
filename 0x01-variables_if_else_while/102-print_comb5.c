688e40949723e2e8a9d253afbbb51248170ff45d
Safe2121 <136501742+Safe2121@users.noreply.github.com>
2023-07-30 02:30:27 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,49 +1,43 @@
-#include <stdio.h> 
- #include <stdlib.h> 
- /** 
-  * main - main block 
-  * Description: Write a program that prints all possible 
-  * different combinations of two digits 
-  * Return: 0 
-  */ 
- int main(void) 
- { 
-         int c = 0; 
-         int f_d; 
-         int l_d; 
-  
-         int c2; 
-         int f_d2; 
-         int l_d2; 
-  
-         while (c <= 98) 
-         { 
-                 f_d = (c / 10 + '0'); 
-                 l_d = (c % 10 + '0'); 
-                 c2 = 0; 
-                 while (c2 <= 99) 
-                 { 
-                         f_d2 = (c2 / 10 + '0'); 
-                         l_d2 = (c2 % 10 + '0'); 
-  
-                         if (c < c2) 
-                         { 
-                                 putchar(f_d); 
-                                 putchar(l_d); 
-                                 putchar(' '); 
-                                 putchar(f_d2); 
-                                 putchar(l_d2); 
-  
-                                 if (c != 98) 
-                                 { 
-                                         putchar(','); 
-                                         putchar(' '); 
-                                 } 
-                         } 
-                         c2++; 
-                 } 
-                 c++; 
-         } 
-         putchar('\n'); 
-         return (0); 
- }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	int tho;
+	int hun;
+	int ten;
+	int bas;
+
+for ( tho = 0 ; tho < 10 ; tho++)
+{	
+
+	for (hun = 0 ; hun <= 10 ; hun++)
+	{
+	
+		for (ten = 0 ; ten < 10 ; ten++)
+		{
+			for ( bas = 0 ; bas < 10 ; bas++)
+			{
+				putchar('0' + tho);
+				putchar('0' + hun);
+				putchar(32);
+				putchar('0' + ten);
+				putchar('0' + bas);
+				if (!(tho == 9 && hun == 8))
+				{
+					putchar(',');
+					putchar(32);
+				}
+				bas++;
+			}
+		}
+	}
+}
+	putchar('\n');
+	return (0);
+}
