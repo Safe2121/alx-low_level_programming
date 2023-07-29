d87842848d608bf6347be297b89f0b6f38f85f9b
Safe2121 <136501742+Safe2121@users.noreply.github.com>
2023-07-30 02:31:34 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,41 +3,18 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Succes)
+ *
+ * Return: Always 0 (Sucess)
  */
 
 int main(void)
 {
-	int tho;
-	int hun;
-	int ten;
-	int bas;
+	char c;
 
-for ( tho = 0 ; tho < 10 ; tho++)
-{	
-
-	for (hun = 0 ; hun <= 10 ; hun++)
+	for (c = 'a' ; c <= 'z' ; c++)
 	{
-	
-		for (ten = 0 ; ten < 10 ; ten++)
-		{
-			for ( bas = 0 ; bas < 10 ; bas++)
-			{
-				putchar('0' + tho);
-				putchar('0' + hun);
-				putchar(32);
-				putchar('0' + ten);
-				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
-				{
-					putchar(',');
-					putchar(32);
-				}
-				bas++;
-			}
-		}
+		putchar(c);
 	}
-}
 	putchar('\n');
 	return (0);
 }
